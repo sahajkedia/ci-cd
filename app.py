f3a08be0eab6a9c771bbf1162f856049e20675e9
Sahaj Kedia <sahajkedia20152498@gmail.com>
2024-10-30 16:46:14 +0530

    Test deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.title("Sample Streamlit App :)")
+st.title("Sample Streamlit App")
 
 
 data = pd.DataFrame(np.random.randn(10, 3), columns=["A", "B", "C"])
