d8872b51a9e13ca7fbea5034197329005599f207
Sahaj Kedia <sahajkedia20152498@gmail.com>
2024-11-06 17:29:18 +0530

    5.7

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.title("A Streamlit Apple")
+st.title("A Streamlit Appleyyy")
 
 
 data = pd.DataFrame(np.random.randn(10, 3), columns=["A", "B", "C"])
